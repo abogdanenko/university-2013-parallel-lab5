ab8646263dec180e15b69878973edacc89182af2
Alexey Bogdanenko <alexey@bogdanenko.com>
2013-09-20 22:49:29 +0400

    Most functionality implemented in sequential program. The program compiles and seems to work.

--- transform-1-qubit.cpp
+++ transform-1-qubit.cpp
@@ -22,6 +22,7 @@
 #include <unistd.h>
 #include <ctype.h>
 #include <iomanip>
+#include <iostream>
 
 using Eigen::Matrix2cd;
 using Eigen::VectorXcd;
@@ -32,6 +33,10 @@ using std::complex;
 using std::string;
 using std::vector;
 using std::stringstream;
+using std::hex;
+using std::cout;
+using std::cerr;
+using std::endl;
 
 typedef complex<double> complexd;
 
@@ -57,24 +62,35 @@ void Transform1Qubit(const VectorXcd& x, const Matrix2cd& U, const int k, Vector
 
 int main(int argc, char** argv)
 {
-    string usage = string("Usage: ") + argv[0] + " -U operator_file "
-        "-x state_vector_file [-k target_qubit] [-t threads] "
-        "-y state_vector_output_file [-T execution_time_output_file]";
+    string usage = string("Usage: ") + argv[0] + " [[-U operator_file] "
+        "{-x state_vector_file | -n random_state_qubit_count} "
+        "[-k target_qubit] [-t threads_count] [-y state_vector_output_file] "
+        "[-T computation_time_output_file]]";
+
+    double T = -1.0; // Transform1Qubit function computation time
 
     VectorXcd x; // initial state vector
     VectorXcd y; // transformed state vector
-    Matrix2cd U; // transform matrix
+    int n = -1; // number of qubits for random state, -1 means 'not specified'
+    // set default values
+    Matrix2cd U = Matrix2cd::Identity(); // transform matrix
     int k = 0; // target qubit index
     int threads_count = 1;
 
     // parse options
+    if (argc == 1)
+    {
+        cout << usage << endl;
+    }
+
+    // NULL means 'not specified by user'
     char* x_filename = NULL;
     char* y_filename = NULL;
     char* U_filename = NULL;
     char* T_filename = NULL;
   
     int c; // option character
-    while ((c = getopt(argc, argv, ":U:x:k:t:y:T:")) != -1)
+    while ((c = getopt(argc, argv, ":U:x:n:k:t:y:T:")) != -1)
     {
         switch(c)
         {
@@ -83,6 +99,9 @@ int main(int argc, char** argv)
                 break;
             case 'x':
                 x_filename = optarg;
+                break;
+            case 'n':
+                n = string_to_int(optarg);
                 break;
             case 'k':
                 k = string_to_int(optarg);
@@ -99,7 +118,8 @@ int main(int argc, char** argv)
             case ':':
                 cerr << "Option -" << optopt << " requires an argument."
                     << endl; 
-                break;
+                cout << usage << endl;
+                return 1;
             case '?':
                 if (isprint(optopt))
                 {
@@ -110,15 +130,32 @@ int main(int argc, char** argv)
                     cerr << "Unknown option character `\\x" << hex << optopt <<
                         "'." << endl;
                 }
+                cout << usage << endl;
                 return 1;
             default:
+                cout << usage << endl;
                 return 1;
         }
     }
 
-    // read x from file
+    if (optind > argc)
     {
-        // ifstream f(x_filename);
+        cerr << "Extra non-option arguments found" << endl;
+        cout << usage << endl;
+        return 1;
+    }
+
+    if (x_filename == NULL && n == -1)
+    {
+        cerr << "State vector filename not specified and number of qubits for "
+            "random state not specified" << endl;
+        cout << usage << endl;
+        return 1;
+    }
+
+    if (x_filename)
+    {
+        // read x from file
         ifstream f(x_filename);
         istream_iterator<complexd> start(f);
         istream_iterator<complexd> eos; // end of stream iterator
@@ -130,9 +167,16 @@ int main(int argc, char** argv)
             x(i) = x_stl[i];
         }
     }
-
-    // read U from file
+    else
     {
+        // set random n qubit state
+        cerr << "not implemented" << endl;
+        return 1;
+    }
+
+    if (U_filename)
+    {
+        // read U from file
         ifstream f(U_filename);
         f >> U(0, 0) >> U(0, 1) >> U(1, 0) >> U(1, 1);
     }
@@ -141,10 +185,18 @@ int main(int argc, char** argv)
 
     Transform1Qubit(x, U, k, y);
 
-    // write y to file
+    if (y_filename)
     {
+        // write y to file
         ofstream f(y_filename);
-        f << y;
+        f << y << endl;
+    }
+
+    if (T_filename)
+    {
+        // write T to file
+        ofstream f(T_filename);
+        f << T << endl;
     }
 
     return 0;
