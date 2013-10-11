030e616af4d2476f52047be87db75e07ff463c73
Alexey Bogdanenko <alexey@bogdanenko.com>
2013-10-11 18:34:55 +0400

    Parser functionality has been encapsulated.

--- transform-1-qubit.cpp
+++ transform-1-qubit.cpp
@@ -83,41 +83,52 @@ inline double random01d(unsigned& m_w, unsigned& m_z)
     return double (r) / 0xffffffff;
 }
 
+class Parser
+{
+    const int argc;
+    char** const argv;
+
+    public:
+
+    class Args
+    {
+        int n; // number of qubits for random state, -1 means 'not specified'
+        int k; // target qubit index
+        // NULL means 'not specified by user', "-" means 'write to stdout'
+        char* x_filename;
+        char* y_filename;
+        char* U_filename;
+        char* T_filename;
+    };
+
+    class ParseError: public runtime_error
+    {
+        public:
+        ParseError(string const& msg);
+    };
+
+    Parser(const int argc, char** const argv);
+    Args Parse();
+    static void PrintUsage();
+};
+
+
 class Transform1Qubit
 {
     vector<complexd> x; // initial state vector
     vector<complexd> y; // transformed state vector
     vector< vector<complexd> > U; // transform matrix
 
-    int n; // number of qubits for random state, -1 means 'not specified'
-    int k; // target qubit index
-    int threads_count;
+    Parser::Args args; // parsed program arguments
+
     // computation time
     double start_time; 
     double end_time; 
 
-    // NULL means 'not specified by user', "-" means 'write to stdout'
-    char* x_filename;
-    char* y_filename;
-    char* U_filename;
-    char* T_filename;
-
     public:
-    class ParseError: public runtime_error
-    {
-        public:
-        ParseError(string const& msg);
-    };
-    
-    class ThreadError: public runtime_error
-    {
-        public:
-        ThreadError();
-    };
-
+   
     Transform1Qubit();
-    void ParseOptions(const int argc, char** const argv);
-    void PrintUsage();
+    void Init(const Parser::Args &args);
     void PrepareInputData();
     void ApplyOperator();
     void WriteResults();
@@ -125,37 +136,24 @@ class Transform1Qubit
     void TimerStop();
 };
 
-Transform1Qubit::ParseError::ParseError(string const& msg):
+Parser::ParseError::ParseError(string const& msg):
     runtime_error(msg)
 {
 
 }
 
-Transform1Qubit::ThreadError::ThreadError():
-    runtime_error("Failed to spawn requested number of threads")
-{
-
-}
- 
-Transform1Qubit::Transform1Qubit():
-    U(vector< vector<complexd> >(2, vector<complexd>(2))),
+Parser::Args::Args():
     n(-1), 
     k(1), 
-    threads_count(1),
     x_filename(NULL),
     y_filename(NULL),
     U_filename(NULL),
     T_filename(NULL)
 {
-    srand(time(NULL));
 
-    U[0][0] = 1;
-    U[0][1] = 0;
-    U[1][0] = 0;
-    U[1][1] = 1;
 }
 
-void Transform1Qubit::PrintUsage()
+void Parser::PrintUsage()
 {
     cout << "Usage: transform-1-qubit [[-U operator_file] "
         "{-x state_vector_file | -n random_state_qubits_count} "
@@ -163,49 +161,7 @@ void Transform1Qubit::PrintUsage()
         "[-T computation_time_output_file]]" << endl;
 }
 
-void Transform1Qubit::TimerStart()
-{
-    start_time = omp_get_wtime();
-}
-
-void Transform1Qubit::TimerStop()
-{
-    end_time = omp_get_wtime();
-}
-
-void Transform1Qubit::ApplyOperator()
-{
-    const int n = intlog2(x.size());
-    const Index mask = 1L << (n - k); // k-th most significant bit
-    y.resize(x.size());
-    const Index N = x.size();
-    
-    int runtime_threads;
-    #pragma omp parallel
-    {
-        #pragma omp single nowait
-        {
-            runtime_threads = omp_get_num_threads();            
-        }
-
-        #pragma omp for
-        for (Index i = 0; i < N; i++)
-        {
-            // bit of i corresponding to k-th qubit ("selected bit")
-            const int i_k = i & mask ? 1 : 0; 
-            const Index i0 = i & ~ mask; // clear selected bit
-            const Index i1 = i | mask; // set selected bit
-            y[i] = U[i_k][0] * x[i0] + U[i_k][1] * x[i1];
-        }
-    }
-    
-    if (runtime_threads < threads_count)
-    {
-        throw ThreadError();
-    }
-}
-
-void Transform1Qubit::ParseOptions(const int argc, char** const argv)
+Parser::Args Parser::Parse()
 {
     ostringstream oss;
     int c; // option character
@@ -262,6 +218,59 @@ void Transform1Qubit::ParseOptions(const int argc, char** const argv)
     {
         throw ParseError("State vector filename not specified and number of "
             "qubits for random state not specified");
+    }
+}
+
+Transform1Qubit::Transform1Qubit():
+    U(vector< vector<complexd> >(2, vector<complexd>(2)))
+{
+    srand(time(NULL));
+
+    U[0][0] = 1;
+    U[0][1] = 0;
+    U[1][0] = 0;
+    U[1][1] = 1;
+}
+
+void Transform1Qubit::TimerStart()
+{
+    start_time = omp_get_wtime();
+}
+
+void Transform1Qubit::TimerStop()
+{
+    end_time = omp_get_wtime();
+}
+
+void Transform1Qubit::ApplyOperator()
+{
+    const int n = intlog2(x.size());
+    const Index mask = 1L << (n - k); // k-th most significant bit
+    y.resize(x.size());
+    const Index N = x.size();
+    
+    int runtime_threads;
+    #pragma omp parallel
+    {
+        #pragma omp single nowait
+        {
+            runtime_threads = omp_get_num_threads();            
+        }
+
+        #pragma omp for
+        for (Index i = 0; i < N; i++)
+        {
+            // bit of i corresponding to k-th qubit ("selected bit")
+            const int i_k = i & mask ? 1 : 0; 
+            const Index i0 = i & ~ mask; // clear selected bit
+            const Index i1 = i | mask; // set selected bit
+            y[i] = U[i_k][0] * x[i0] + U[i_k][1] * x[i1];
+        }
+    }
+    
+    if (runtime_threads < threads_count)
+    {
+        throw ThreadError();
     }
 }
 
@@ -386,7 +395,7 @@ void Master::Run()
 {
     if (argc == 1)
     {
-        PrintUsage();
+        Parser::PrintUsage();
         AbortWorkers();
     }
     else
@@ -394,7 +403,7 @@ void Master::Run()
         Timer.Start();
         DistributeInputData();
         ManageLocalWorker();
-        ReceiveWriteResults();
+        ReceiveAndWriteResults();
         Timer.Stop();
         WriteComputationTime();
     }
@@ -414,27 +423,25 @@ void Worker::Run()
 int main(int argc, char** argv)
 {
     int rank;
-    int size; // number of processes
     int exit_code = EXIT_SUCCESS;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
     if (rank == 0)
     {
         Parser parser(argc, argv);
-        Master master(size);
+        Master master();
         try
         {
-            parse_struct = parser.Parse();
-            master.Init(parse_struct, size);
+            Parser::Args args = parser.Parse();
+            master.Init(args);
             master.Run();
         }
         catch (Parser::ParseError& e)
         {
             cerr << e.what() << endl;
-            parser.PrintUsage();
+            Parser::PrintUsage();
             master.AbortWorkers();
             exit_code = EXIT_FAILURE;
         }
