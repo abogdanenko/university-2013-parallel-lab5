7654c2e09b6fae916f333f47325deb0c501fe6eb
Alexey Bogdanenko <alexey@bogdanenko.com>
2013-10-12 17:26:42 +0400

    class RandomComplexGenerator has been moved to a separate file.

---
+++ randomcomplexgenerator.cpp
@@ -0,0 +1,29 @@
+/*
+    Pseudo-random number generator
+
+    Returns real numbers between 0 and 1
+    copied from
+    http://en.wikipedia.org/wiki/Random_number_generation#cite_ref-6
+*/
+double RandomComplexGenerator::random01d()
+{
+    m_z = 36969 * (m_z & 65535) + (m_z >> 16);
+    m_w = 18000 * (m_w & 65535) + (m_w >> 16);
+    unsigned r = (m_z << 16) + m_w;  // 32-bit result 
+    return double (r) / 0xffffffff;
+}
+
+RandomComplexGenerator::RandomComplexGenerator():
+    next_seed1(rand()),
+    next_seed2(rand())
+{
+}
+
+complexd RandomComplexGenerator::operator()()
+{
+    double re = random01d() - 0.5;
+    double im = random01d() - 0.5;
+    return complexd(re, im);
+}
+
+
---
+++ randomcomplexgenerator.h
@@ -0,0 +1,10 @@
+class RandomComplexGenerator()
+{
+    unsigned m_w;
+    unsigned m_z;
+    inline double random01d()
+    public:
+    RandomComplexGenerator();
+    static complexd operator()();
+};
+
