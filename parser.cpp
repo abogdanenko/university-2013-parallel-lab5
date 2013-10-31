ab7db0a15cc8d9d710e479f4eb38d40077a8821f
Alexey Bogdanenko <alexey@bogdanenko.com>
2013-10-31 21:39:06 +0400

    Parser has been updated to handle new argument set.

--- parser.cpp
+++ parser.cpp
@@ -1,23 +1,15 @@
 #include <iostream>
-#include <sstream> // stringstream, ostringstream
+#include <sstream> // ostringstream
 #include <cctype>
 #include <unistd.h> // getopt, optind, optarg
 
 #include "parser.h"
+#include "routines.h"
 
 using std::cout;
 using std::endl;
 using std::hex;
-using std::stringstream;
 using std::ostringstream;
-
-int string_to_int(const string& s)
-{
-    int n;
-    stringstream ss(s);
-    ss >> n;
-    return n;
-}
 
 Parser::ParseError::ParseError(const string& msg):
     runtime_error(msg)
@@ -34,10 +26,13 @@ Parser::Parser(const int argc, char** const argv):
 
 void Parser::PrintUsage()
 {
-    cout << "Usage: transform-1-qubit-mpi [-n qubit_count "
-        "[-U operator_file] [-x state_vector_file] "
-        "[-k target_qubit] [-y state_vector_output_file] "
-        "[-t computation_time_output_file]]" << endl;
+    cout << "Usage: fidelity-mpi ["
+            "-n qubit_count "
+            "-e epsilon"
+            "[-i iteration_count] "
+            "[-f fidelity_output_file] "
+            "[-t computation_time_output_file]
+        "]" << endl;
 }
 
 Args Parser::Parse()
@@ -45,24 +40,21 @@ Args Parser::Parse()
     Args result;
     ostringstream oss;
     int c; // option character
-    while ((c = getopt(argc, argv, ":U:x:n:k:t:y:t:")) != -1)
+    while ((c = getopt(argc, argv, ":n:e:i:f:t:")) != -1)
     {
         switch(c)
         {
-            case 'U':
-                result.matrix_filename = optarg;
-                break;
-            case 'x':
-                result.vector_input_filename = optarg;
-                break;
             case 'n':
-                result.qubit_count = string_to_int(optarg);
+                result.qubit_count = string_to_number<int>(optarg);
                 break;
-            case 'k':
-                result.target_qubit = string_to_int(optarg);
+            case 'e':
+                result.epsilon = string_to_number<double>(optarg);
                 break;
-            case 'y':
-                result.vector_output_filename = optarg;
+            case 'i':
+                result.iteration_count = string_to_number<int>(optarg);
+                break;
+            case 'f':
+                result.fidelity_filename = optarg;
                 break;
             case 't':
                 result.computation_time_filename = optarg;
@@ -94,6 +86,11 @@ Args Parser::Parse()
     if (result.qubit_count == -1)
     {
         throw ParseError("Number of qubits not specified");
+    }
+
+    if (result.epsilon == -1.0)
+    {
+        throw ParseError("Epsilon not specified");
     }
     return result;
 }
--- routines.h
+++ routines.h
@@ -1,6 +1,10 @@
 #ifndef ROUTINES_H
 #define ROUTINES_H
 
+#include <sstream> // stringstream
+
+using std::stringstream;
+
 // for n = 2**m returns m
 template <class Integer>
 int intlog2(const Integer n)
@@ -10,6 +14,15 @@ int intlog2(const Integer n)
     while (t >>= 1)
       result++;
     return result;
+}
+
+template <class Number>
+Number string_to_number(const string& s)
+{
+    Number n;
+    stringstream ss(s);
+    ss >> n;
+    return n;
 }
 
 #endif
