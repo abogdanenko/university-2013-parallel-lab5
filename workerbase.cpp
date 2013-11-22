bbc2e0e96b851e153ba8de2981a0c26fcef22f99
Alexey Bogdanenko <alexey@bogdanenko.com>
2013-11-22 14:59:53 +0400

    Update WorkerBase::NormalizeGlobal

--- workerbase.cpp
+++ workerbase.cpp
@@ -48,17 +48,15 @@ void WorkerBase::VectorInitRandom()
 
 void WorkerBase::NormalizeGlobal()
 {
-    long double local_sum = 0.0;
+    double sum = 0.0;
     for (auto it = psi.begin(); it != psi.end(); it++)
     {
-        local_sum += norm(*it);
+        sum += norm(*it);
     }
 
-    long double global_sum;
-    MPI_Allreduce(&local_sum, &global_sum, 1, MPI_LONG_DOUBLE, MPI_SUM,
-        MPI_COMM_WORLD);
+    shmem_double_allsum(&sum);
 
-    const complexd coef = 1.0 / sqrt(global_sum);
+    const complexd coef = 1.0 / sqrt(sum);
     // multiply each element by coef
     transform(psi.begin(), psi.end(), psi.begin(),
         bind1st(multiplies<complexd>(), coef));
