99cdeff8ed82743fcb523d3a501f95d3044b03a1
edu-vmk13-studalex01_251399 <edu-vmk13-studalex01_251399@compiler-2.lomonosov.parallel.ru>
2013-11-25 15:28:08 +0400

    Simplify debug header

--- debug.h
+++ debug.h
@@ -3,21 +3,18 @@
 
 #ifdef DEBUG
 #include <iostream>
-#endif
 
-#ifdef DEBUG
 #define INDENT_WIDTH 4
 #define INDENT_CHAR ' '
 #define INDENT(level) string((level) * INDENT_WIDTH, INDENT_CHAR)
-#endif
 
-#ifdef DEBUG
 using std::string;
 using std::cout;
 using std::endl;
-#endif
 
 #ifdef WAITFORGDB
 void WaitForGdb();
 #endif
-#endif
+
+#endif // DEBUG
+#endif // DEBUG_H
