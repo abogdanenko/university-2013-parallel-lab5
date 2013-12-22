6273312f7a24fa6c43f0759dc0a2631945b0f34f
Alexey Bogdanenko <alexey@bogdanenko.com>
2013-12-23 00:51:53 +0400

    Add ability to start/stop timer multiple times
    - Checkout timer.h and timer.cpp from lab05v1.0dislib
    - Replace dislib calls with mpi calls

--- timer.cpp
+++ timer.cpp
@@ -2,19 +2,27 @@
 
 #include "timer.h"
 
+Timer::Timer():
+    sum(0.0)
+{
+
+}
+
 void Timer::Start()
 {
+    MPI_Barrier(MPI_COMM_WORLD);
     start = MPI_Wtime();
 }
 
 void Timer::Stop()
 {
-    end = MPI_Wtime();
+    MPI_Barrier(MPI_COMM_WORLD);
+    const double end = MPI_Wtime();
+    const double delta = end - start;
+    sum += delta;
 }
 
-double Timer::GetDelta() const
+double Timer::Total() const
 {
-    return end - start;
+    return sum;
 }
-
-
